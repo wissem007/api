dcf8bed62858e9a87a848eb24c63fde2f8748cc9
wissem007 <alouiwiss@gmail.com>
2024-01-31 12:03:40 +0100

    deuxieme commit

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,16 @@
-# Utilisez une image Node.js comme base
 FROM node:14-alpine
 
-# Définissez le répertoire de travail dans le conteneur
 WORKDIR /usr/src/app
 
-# Copiez les fichiers package.json et package-lock.json dans le répertoire de travail
 COPY package*.json ./
 
-# Installez les dépendances de l'application
 RUN npm install
 
-# Copiez le reste des fichiers de l'application dans le répertoire de travail
 COPY . .
 
-# Exposez le port sur lequel l'application s'exécute
+# Définissez l'URL de votre API en tant que variable d'environnement
+ENV API_URL=http://178.33.44.117
+
 EXPOSE 3000
 
-# Commande pour exécuter l'application
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-// server.js
-const express = require('express');
-const { Pool } = require('pg');
-
-// Création de l'application Express
-const app = express();
-const port = 3000;
-
-// Configuration de la connexion à la base de données PostgreSQL
-const pool = new Pool({
-  user: 'postgres',
-  host: '192.168.1.120',
-  database: 'ftf_2022',
-  password: 'root',
-  port: 5432,
-});
-
-// Route pour récupérer des informations à partir de la base de données
 app.get('/informations/:licence_num', async (req, res) => {
   const { licence_num } = req.params;
   
   try {
     const result = await pool.query(
       `SELECT 
-        ct_intervenant_id,
-        name,
-        last_name,
-        cin_number,
-        passport_num,
-        date_of_birth,
-        place_of_birth,
-        licence_num
-      FROM 
-        sss_competition_db.ct_intervenants 
+          ct_team_intervenant_id, 
+          ct_intervenant_id,
+          name,
+          last_name,
+          cin_number,
+          passport_num,
+          date_of_birth,
+          place_of_birth,
+          licence_num,
+          ct_season_id
+      FROM (
+          SELECT 
+              t.ct_team_intervenant_id, 
+              i.ct_intervenant_id,
+              i.name,
+              i.last_name,
+              i.cin_number,
+              i.passport_num,
+              i.date_of_birth,
+              i.place_of_birth,
+              i.licence_num,
+              t.ct_season_id,
+              ROW_NUMBER() OVER (PARTITION BY i.ct_intervenant_id ORDER BY t.ct_season_id DESC) AS row_num
+          FROM 
+              sss_competition_db.ct_intervenants i
+          JOIN 
+              sss_competition_db.ct_team_intervenants t
+          ON 
+              i.ct_intervenant_id = t.ct_intervenant_id
+          WHERE 
+              i.licence_num = $1
+      ) AS subquery
       WHERE 
-        licence_num = $1`, 
+          row_num = 1`, 
       [licence_num]
     );
     res.json(result.rows);
@@ -41,8 +45,4 @@ app.get('/informations/:licence_num', async (req, res) => {
     console.error('Erreur lors de la récupération des informations:', error);
     res.status(500).json({ error: 'Erreur interne du serveur' });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Serveur API en cours d'exécution sur le port ${port}`);
 });
